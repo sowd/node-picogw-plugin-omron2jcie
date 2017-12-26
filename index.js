e5e8ad93689e1eeea28e387f2cfeb53ad24b9194
Satoshi Ikeda <ikeda@koozyt.com>
2017-12-26 21:32:58 +0900

    Apply coding style and Implement new plugin architecture

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var fs = require('fs');
-const pathm = require('path');
-const LocalStorage = require('node-localstorage').LocalStorage;
-const MYPATH  = __filename.split(pathm.sep).slice(0,-1).join('/') ;
-const DATAPATH = MYPATH+'/data' ;
-const localStorage = new LocalStorage( DATAPATH ) ;
-
-let pluginInterface ;
-let log = console.log ;
+let pi;
+let log = console.log; // eslint-disable-line no-unused-vars
+let localStorage;
 
 module.exports = {
     init: init,
@@ -14,66 +8,70 @@ module.exports = {
 };
 
 
-function init(pi){
-	pluginInterface = pi ;
-	log = pi.log ;
-	//pi.on('SettingsUpdated' , newSettings =>{} ) ;
-} ;
+/**
+ * Initialize plugin
+ * @param {object} pluginInterface The interface of picogw plugin
+ */
+function init(pluginInterface) {
+    pi = pluginInterface;
+    log = pi.log;
+    localStorage = pi.localStorage;
+}
 
-function onProcCall( method , path , args ){
-	let re ;
-	switch(method){
-	case 'GET' :
-		if (path==''){	// Request for members
-			try {
-				fs.statSync( DATAPATH ) ;
-				return new Promise( (rslv,rjct)=>{
-					fs.readdir( DATAPATH, (err, files) => {
-						re = {} ;
-						files.forEach(fname => {
-							try {
-								let fo = fs.lstatSync(`${DATAPATH}/${fname}`) ;
-								if( fo.isDirectory() ) return ;
-							} catch(e){ return ; }
+/**
+ * onCall handler of plugin
+ * @param {string} method Caller method, accept GET only.
+ * @param {string} path Plugin URL path
+ * @param {object} args parameters of this call
+ * @return {object} Returns a Promise object or object containing the result
+ */
+function onProcCall(method, path, args) {
+    let re;
+    switch (method) {
+    case 'GET':
+        if (path === '') { // Request for members
+            return listKeys(args);
+        }
+        re = localStorage.getItem(path);
+        return (re == null ? {error: 'No such path:'+path} : JSON.parse(re));
+    case 'POST':
+    case 'PUT':
+        try {
+            localStorage.setItem(path, JSON.stringify(args));
+            pi.publish(path, args); // PubSub
+            return {success: true};
+        } catch (e) {
+            return {error: 'Data should be in JSON format.'};
+        }
+    case 'DELETE':
+        if (path == '') localStorage.clear();
+        else localStorage.removeItem(path);
+        pi.publish(path, {}); // PubSub
+        return {success: true};
+    default:
+        return {error: `The specified method ${method} is not implemented in admin plugin.`}; // eslint-disable-line max-len
+    }
+}
 
-							let fname_mid = decodeURIComponent(fname) ;
-							fname = decodeURIComponent(fname_mid) ;
-							re[fname] = {} ;
-							if( args && args.option === 'true' ){
-								let gi = localStorage.getItem(fname_mid);
-								re[fname].option = {
-									doc : {
-										short : localStorage.getItem(fname_mid).length+' byte'
-										//,long : 'Optional long message'
-									}
-									, leaf : true
-								} ;
-							}
-						}) ;
-						rslv(re) ;
-					} ) ;
-				} ) ;
-			} catch(e){
-				return {error:'Error in accessing '+DATAPATH} ;
-			}
-		}
-		re = localStorage.getItem(path) ;
-		return ( re == null ? {error:'No such path:'+path} : JSON.parse(re) ) ;
-	case 'POST' :
-	case 'PUT' :
-		try {
-			localStorage.setItem(path,JSON.stringify( args )) ;
-			pluginInterface.publish(path,args) ;	// PubSub
-			return {success:true} ;
-		} catch(e){
-			return {error:'Data should be in JSON format.'} ;
-		}
-	case 'DELETE' :
-		if(path == "" )	localStorage.clear() ;
-		else			localStorage.removeItem(path) ;
-		pluginInterface.publish(path,{}) ;	// PubSub
-		return {success:true} ;
-	default :
-		return {error:`The specified method ${method} is not implemented in admin plugin.`} ;
-	}
+
+// eslint-disable-next-line require-jsdoc
+function listKeys(args) {
+    return new Promise((rslv) => {
+        const re = {};
+        for (let i=0; i<localStorage.length; i++) {
+            const key = localStorage.key(i);
+            re[key] = {};
+            if (args && args.option === 'true') {
+                const size = localStorage.getItem(key).length;
+                re[key].option = {
+                    doc: {
+                        short: size + ' byte',
+                        // ,long : 'Optional long message'
+                    },
+                    leaf: true,
+                };
+            }
+        }
+        rslv(re);
+    });
 }
